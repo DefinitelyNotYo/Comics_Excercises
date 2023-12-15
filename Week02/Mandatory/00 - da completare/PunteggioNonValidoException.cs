7eea17f059cae4a5efd2f09ad230fcae1eb26f0a
DefinitelyNotYo <151754586+DefinitelyNotYo@users.noreply.github.com>
2023-12-15 15:01:30 +0100

    Add files via upload

---
+++ Week02/Mandatory/00 - da completare/PunteggioNonValidoException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class MiaEccezione : Exception
+    {
+        public MiaEccezione() 
+        {
+            Console.ForegroundColor = ConsoleColor.Red; Console.Write("\nIl valore che hai inserito non è valido. Inserisci un numero compreso fra 1 e 10 \n"); Console.ResetColor();
+
+        }
+        public MiaEccezione(string message) : base(message) { }
+    }
+}
